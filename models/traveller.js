57f2f9d07c087038f6fb95ef223674775aa38ce8
jjardine85 <jamesjardine@gmx.com>
2019-01-30 16:01:37 +0000

    homework

---
+++ models/traveller.js
@@ -0,0 +1,38 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  const result = this.journeys.filter(journey => journey.transport === transport);
+  return result;
+} ;
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  const result = this.journeys.filter(journey => journey.distance > minDistance);
+  return result
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
